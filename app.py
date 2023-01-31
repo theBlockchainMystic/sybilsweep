b05f47e17d1e190c044a97946e7aea0375a1991e
theBlockchainMystic <123568497+theBlockchainMystic@users.noreply.github.com>
2023-01-31 10:32:45 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ import pandas as pd
 import json
 import pickle
 import numpy as np
-from eth_abi import abi
 
 app = Flask(__name__)
-apikey = "" ##create etherscan api key
 
 # load saved model Isolation Forest
 with open('models/isolation_forest' , 'rb') as f:
@@ -18,54 +16,6 @@ with open('models/isolation_forest' , 'rb') as f:
 with open('models/robust_cov' , 'rb') as f:
     cov = pickle.load(f)
 
-## get_block num
-def get_block(timestamp):
-    r = requests.post(f"https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp={timestamp}&closest=before&apikey={apikey}")
-    results=r.json()
-    return results['result']
-
-
-## Normal Transactions By Address
-def feature_store(start_block_num,end_block_num,wallet_address):
-    normal_trans = f"https://api.etherscan.io/api?module=account&action=txlist&address={wallet_address}&startblock={start_block_num}&endblock={end_block_num}&page=1&offset=10&sort=asc&apikey={apikey}"
-    r = requests.post(normal_trans)
-    print(wallet_address,r)
-    results=r.json()
-    #print(results['result'])
-    if len(results['result'])>0:
-        try:
-            l=pd.DataFrame(results['result'])
-        except:
-            l=None
-    else:
-        l=None
-    
-    return l
-
-## checks if wallets is used only for voting
-def vote_only(start_block_num,end_block_num,source_wallets):
-    results=[]
-    print("###### Obtaining Wallet meta-data ######")
-    for i in source_wallets:
-        r=feature_store(start_block_num,end_block_num,i)
-        if r is None:
-            pass
-        else:
-            results.append(r)
-    if len(results)>0:
-        #print(results)
-        temp_df=pd.concat(results)
-        #print(temp_df)
-        vl=list(temp_df[temp_df['functionName'].str.contains('vote')]['functionName'].dropna().unique())
-        temp_df['classes'] = 1
-        temp_df['classes']=temp_df['classes'].where(temp_df['functionName'].isin(vl),0)
-        s1=pd.concat([temp_df.groupby(['from'])['classes'].count(),temp_df.groupby(['from'])['classes'].sum()],axis=1)
-        s1=s1.reset_index()
-        s1.columns = ['from','total','vote']
-        s1['percent_vote'] = s1['vote']*100/s1['total']
-        return list(s1[s1['percent_vote']==100]['from'].unique())
-    else:
-        return []
 
 def ss(df):
     if df['amount'].std()>0:
@@ -88,11 +38,10 @@ def prepocess(data):
     data=data.drop(columns='index')
     return data
 
-@app.route('/sybil',methods=['POST'])
+@app.route('/sybilsweep',methods=['POST'])
 def pipe():
     inp_json = request.json
     df = pd.DataFrame.from_dict(inp_json)
-    source_wallets=list(set(df.source_wallet.unique()))
     ## Prediction
     df = prepocess(df)
     X = df[['ss_amount','time_diff']].values
@@ -103,15 +52,6 @@ def pipe():
     df['robust_cov_prediction']=cov.predict(X)
     df['robust_cov_prediction']=df['robust_cov_prediction'].map({-1: 'True', 1: 'False'})
     df=df[['id', 'token', 'amount', 'source_wallet', 'destination_wallet','created_at', 'isolation_forest_prediction','isolation_forest_score','robust_cov_prediction','robust_cov_score']]
-    ## Wallet check
-    start_epoch = int(df.created_at.min().value/1e9)
-    end_epoch = int(df.created_at.max().value/1e9)
-    print('start_epoch: ',start_epoch, 'end_epoch: ',end_epoch)
-    start_block_num = get_block(start_epoch)
-    end_block_num = get_block(end_epoch)
-    votes_only = vote_only(start_block_num,end_block_num,source_wallets)
-    df['sybil_account'] = 'True'
-    df['sybil_account']=df['sybil_account'].where(df['source_wallet'].isin(votes_only),'False')
     return df.to_json()
 
 if __name__ == '__main__':
